cfab6b8d75c44fa3de8762881b32dade9beafb43
robbsky14 <rz14687@gmail.com>
2020-05-13 19:27:49 +0200

    subclass Coffee added

---
+++ src/main/java/at/fhj/iit/Coffee.java
@@ -0,0 +1,28 @@
+package at.fhj.iit;
+
+public class Coffee extends Drink {
+
+	public Coffee(String name) {
+		super(name);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public double getVolume() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public double getAlcoholPercent() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public boolean isAlcoholic() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
